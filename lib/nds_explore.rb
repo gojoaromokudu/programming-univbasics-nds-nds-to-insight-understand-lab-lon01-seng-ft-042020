3263d78403bb82ba6fb5cfa6f2bdf1a9b679f769
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-01 17:25:27 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ end
 
 def print_first_directors_movie_titles
 
-pp directors_database
+
 
 row_index = 0 
 while row_index < directors_database[0][:movies].length do
@@ -17,6 +17,6 @@ puts directors_database[0][:movies][row_index][:title]
 row_index += 1
 end
 
-puts directors_database[0][:movies].length
+
 
 end
